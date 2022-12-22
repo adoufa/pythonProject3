41ccf42ad17507477a6322eecff7da8e14989c64
adoufa <arinadolgaya@icloud.com>
2022-12-22 20:38:15 +0300

    2 commit

---
+++ client.py
@@ -0,0 +1,35 @@
+import socket, threading
+
+
+nickname = input('Введите имя пользователя: ')
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('127.0.0.1', 2327))
+
+
+def receive():
+    while True:  # Подтверждение соединения
+        try:
+            message = client.recv(1024).decode('utf-8')
+            if message == 'NICKNAME':
+                client.send(nickname.encode('utf-8'))
+            else:
+                print(message)
+
+        except:  # Если неправильрный ip или порт
+            print("Ошибка!")
+            client.clode()
+            break
+
+
+
+def write():
+    while True:  # Вывод сообщений в чат
+        message = '{}:{}'.format(nickname, input(''))
+        client.send(message.encode('utf-8'))
+
+
+receive_thread = threading.Thread(target=receive)
+receive_thread.start()
+write_thread = threading.Thread(target=write)
+write_thread.start()
